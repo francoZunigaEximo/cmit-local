62a76288fb5b537adc6518bf68c8444b14b34e87
Nicolas Maximowicz <nmaximowicz@eximo.com.ar>
2023-10-23 07:56:44 -0300

    Fix: Provincia

--- app/Http/Controllers/ProveedoresController.php
+++ app/Http/Controllers/ProveedoresController.php
@@ -25,7 +25,7 @@ class ProveedoresController extends Controller
         $detalleProv = Provincia::find($localidad->Id);
         $provincias = Provincia::all();
 
-        return view('layouts.especialidades.edit', compact(['especialidade', 'detalleProv']));
+        return view('layouts.especialidades.edit', compact(['especialidade', 'detalleProv', 'provincias']));
     }
 
     public function create()
