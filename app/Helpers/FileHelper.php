8a2b3ea4bb5277215bee7ce20a346b682b1addd8
Nicolas Maximowicz <nmaximowicz@eximo.com.ar>
2024-08-28 19:16:18 -0300

    Test: SMB pruebas

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -14,16 +14,7 @@ class FileHelper
 
         if ($disk  === 'smb') {
             
-            try {
-                $serverFactory = new ServerFactory();
-                $auth = new BasicAuth(env('USER_SMB'), env('SHARE_SMB'), env('PASS_SMB'));
-                $server = $serverFactory->createServer(env('HOST_SMB'), $auth);
-
-                return $server->getShare('ROOT_SMB');
-
-            } catch( Exception $e) {
-                return response()->json(['msg' => 'Error SMB: ' . $e->getMessage()], 502);
-            }
+            return 'http://localhost:8005/';
 
         } else {
             return $type === 'lectura' ? asset('storage') : storage_path('app/public');
