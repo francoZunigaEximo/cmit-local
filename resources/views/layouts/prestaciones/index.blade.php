fb0416f13013a0f798aaefe3d6572248e4b2f06d
Nicolas Maximowicz <nmaximowicz@eximo.com.ar>
2024-09-16 08:40:26 -0300

    "Chore: ajustes"

--- resources/views/layouts/prestaciones/index.blade.php
+++ resources/views/layouts/prestaciones/index.blade.php
@@ -325,7 +325,7 @@ function exportExcel(tipo) {
 
 @push('scripts')
 <!--datatable js-->
-<script src="https://cdn.datatables.net/2.1.6/js/dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
 
