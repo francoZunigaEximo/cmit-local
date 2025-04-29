2f000c89ada28fe8f3b65d7a9b1ba27f6cacff2a
francoZunigaEximo <fzuniga@eximo.com.ar>
2025-04-29 14:38:03 -0300

    feat: reportes del 17 al 28

--- app/Enum/ListadoReportes.php
+++ app/Enum/ListadoReportes.php
@@ -17,6 +17,18 @@ use App\Services\Reportes\Estudios\EXAMENREPORTE13;
 use App\Services\Reportes\Estudios\EXAMENREPORTE14;
 use App\Services\Reportes\Estudios\EXAMENREPORTE15;
 use App\Services\Reportes\Estudios\EXAMENREPORTE16;
+use App\Services\Reportes\Estudios\EXAMENREPORTE17;
+use App\Services\Reportes\Estudios\EXAMENREPORTE18;
+use App\Services\Reportes\Estudios\EXAMENREPORTE19;
+use App\Services\Reportes\Estudios\EXAMENREPORTE20;
+use App\Services\Reportes\Estudios\EXAMENREPORTE21;
+use App\Services\Reportes\Estudios\EXAMENREPORTE22;
+use App\Services\Reportes\Estudios\EXAMENREPORTE23;
+use App\Services\Reportes\Estudios\EXAMENREPORTE24;
+use App\Services\Reportes\Estudios\EXAMENREPORTE25;
+use App\Services\Reportes\Estudios\EXAMENREPORTE26;
+use App\Services\Reportes\Estudios\EXAMENREPORTE27;
+use App\Services\Reportes\Estudios\EXAMENREPORTE28;
 
 class ListadoReportes
 {
@@ -39,7 +51,19 @@ class ListadoReportes
             13 => EXAMENREPORTE13::class,
             14 => EXAMENREPORTE14::class,
             15 => EXAMENREPORTE15::class,
-            16 => EXAMENREPORTE16::class
+            16 => EXAMENREPORTE16::class,
+            17 => EXAMENREPORTE17::class,
+            18 => EXAMENREPORTE18::class,
+            19 => EXAMENREPORTE19::class,
+            20 => EXAMENREPORTE20::class,
+            21 => EXAMENREPORTE21::class,
+            22 => EXAMENREPORTE22::class,
+            23 => EXAMENREPORTE23::class,
+            24 => EXAMENREPORTE24::class,
+            25 => EXAMENREPORTE25::class,
+            26 => EXAMENREPORTE26::class,
+            27 => EXAMENREPORTE27::class,
+            28 => EXAMENREPORTE28::class,
         ];
 
         return $examenes[$id];
