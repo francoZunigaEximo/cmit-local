7bb05500126bf1b5efca5d001907f97b01aaf1cf
Nicolas Maximowicz <nmaximowicz@eximo.com.ar>
2025-08-19 20:36:55 -0300

    "Fix: problema con socket mal ubicado solo en test"

--- public/js/llamador/efector/index.js
+++ public/js/llamador/efector/index.js
@@ -368,36 +368,6 @@ $(function(){
         }
     }
 
-    echo.listen('.ListadoProfesionalesEvent', (response) => {
-        const efectores = response.efectores;
-
-        $('#profesional').empty();
-
-        toastr.info('Se ha actualizado la lista de profesionales', '', {timeOut: 1000});
-
-        if (efectores.length === 1) {
-
-            $('#profesional').append(
-                `<option value="${efectores[0].Id}" selected>${efectores[0].NombreCompleto}</option>`
-            );
-        } else if(efectores.length > 1) {
-
-            $('#profesional').append('<option value="" selected>Elija una opción...</option>');
-            $.each(efectores, function(index, value){
-                let contenido = `<option value="${value.Id}">${value.NombreCompleto}</option>`;
-
-                $('#profesional').append(contenido);
-            });
-
-        } else {
-
-            $('#profesional').append(
-                `<option value="" selected>No hay efectores</option>`
-            );
-        }
-    });
-
-   
     function verificarProfesional(data, tipoProfesional) {
         if(data.length === 0) return;
 
