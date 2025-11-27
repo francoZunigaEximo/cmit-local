d2f9db19bd899bce884a15d76b479606d6fed156
francoZunigaEximo <fzuniga@eximo.com.ar>
2025-11-27 11:34:09 -0300

    fix: datos paciente en resultados

--- app/Http/Controllers/PrestacionesController.php
+++ app/Http/Controllers/PrestacionesController.php
@@ -154,8 +154,8 @@ class PrestacionesController extends Controller
         $auditorias = Auditor::with('auditarAccion')->where('IdTabla', 1)->where('IdRegistro', $prestacione->Id)->orderBy('Id', 'Asc')->get();
         $fichalaboral = Fichalaboral::where('IdPaciente', $prestacione->IdPaciente)->orderBy('Id', 'Desc')->first();
         $tiposPrestacionOtros = PrestacionesTipo::whereNotIn('Nombre', $tiposPrestacionPrincipales)->get();
-
-        return view('layouts.prestaciones.edit', compact(['tipoPrestacion', 'prestacione', 'financiador', 'auditorias', 'fichalaboral', 'tiposPrestacionOtros']), ['helper'=> $this->helperEdit]);
+        $paciente = Paciente::find($prestacione->IdPaciente, ['Nombre', 'Apellido', 'Documento', 'TipoDocumento']);
+        return view('layouts.prestaciones.edit', compact(['tipoPrestacion', 'prestacione', 'financiador', 'auditorias', 'fichalaboral', 'tiposPrestacionOtros', 'paciente']), ['helper'=> $this->helperEdit]);
     }
 
     public function estados(Request $request)
