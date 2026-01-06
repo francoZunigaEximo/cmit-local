3fbb1b9d79eea62c176b564b77f453abe7ad4e29
francoZunigaEximo <fzuniga@eximo.com.ar>
2026-01-06 14:10:46 -0300

    fix: btn warning volver en crear especialidad

--- resources/views/layouts/especialidades/create.blade.php
+++ resources/views/layouts/especialidades/create.blade.php
@@ -6,7 +6,7 @@
 
 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
     <h4 class="mb-sm-0">Especialidad <span class="badge text-bg-primary">Nueva</span></h4>
-    <a class="btn btn-sm botonGeneral" href="{{ route('especialidades.index')}}">Volver</a>
+    <a class="btn btn-warning botonGeneral" href="{{ route('especialidades.index')}}">Volver</a>
 </div>
 
 <div class="card-header">
