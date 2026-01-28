d9cce39fec6a1b63f229e891c650a347fbdfd432
francoZunigaEximo <fzuniga@eximo.com.ar>
2026-01-28 08:48:55 -0300

    feat: readme con info sobre ejecucion de contenedores docker

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -5,10 +5,11 @@ set -e
 USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 
-# Fix file ownership and permissions using the passed UID and GID
-echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
-#chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
-
+#cambiamos los permisos en caso de estar en un SO linux
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
+    chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
+fi
 # Clear configurations to avoid caching issues in development
 cd /var/www
 
