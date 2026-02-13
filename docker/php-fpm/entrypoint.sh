7c73b48b9926f2a88dca9ea9c6470e012fb45105
francoZunigaEximo <fzuniga@eximo.com.ar>
2026-02-13 10:19:56 -0300

    cambios a descartar

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -1,15 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Check if $UID and $GID are set, else fallback to default (1000:1000)
 USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 
-#cambiamos los permisos en caso de estar en un SO linux
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
-    chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
-fi
 # Clear configurations to avoid caching issues in development
 cd /var/www
 
